e5d6ceb9f20e3455d89e0982ace6ddd2b0d233f0
biblioJouets <contact@bibliojouets.com>
2025-11-24 10:15:08 +0100

    modify permissive csp

--- next.config.mjs
+++ next.config.mjs
@@ -25,20 +25,34 @@ const nextConfig = {
           // ---------------------------
           //  CONTENT SECURITY POLICY
           // ---------------------------
-{
+ {
+
   key: "Content-Security-Policy",
+
   value: [
+
     "default-src 'self'",
-    "script-src 'self' https://hcaptcha.com https://*.hcaptcha.com 'unsafe-inline'",
-    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com",
-    "img-src 'self' data: https:",
+
+    "script-src 'self' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com",
+
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com https://hcaptcha.com https://*.hcaptcha.com",
+
+    "img-src 'self' data: https: https://hcaptcha.com https://*.hcaptcha.com",
+
     "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com",
+
     "connect-src 'self' https://hcaptcha.com https://*.hcaptcha.com https://sentry.hcaptcha.com",
+
     "frame-src https://hcaptcha.com https://*.hcaptcha.com",
+
     "frame-ancestors 'none'",
+
     "base-uri 'self'",
+
     "form-action 'self'"
+
   ].join("; ")
+
 }
         ]
       }
